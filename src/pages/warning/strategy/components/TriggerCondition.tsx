e3b3ce07ab07bc3c3898931b14505df84c9b91af
Szq20 <shenzhiqiang_i@didiglobal.com>
2021-09-10 11:01:18 +0800

    fix:新建告警策略->修改了m的值，下面的提示没有变化

--- src/pages/warning/strategy/components/TriggerCondition.tsx
+++ src/pages/warning/strategy/components/TriggerCondition.tsx
@@ -168,6 +168,7 @@ const TriggerConditon = (props) => {
       let params = form.getFieldValue('trigger_conditions')[field.name]?.params;
       mVal = params.length > 0 && params[0] !== 1 ? params[0] / 86400 : 1;
     } else {
+      let params = form.getFieldValue('trigger_conditions')[field.name]?.params;
       mVal = params[0] || 1;
     }
 
