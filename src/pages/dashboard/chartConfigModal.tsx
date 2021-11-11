24abe301c8b7143f86af7b0f0fe4b01f45013cda
guguji5 <dukuan@didiglobal.com>
2021-11-11 20:31:29 +0800

    fix: build error fix

--- src/pages/dashboard/chartConfigModal.tsx
+++ src/pages/dashboard/chartConfigModal.tsx
@@ -374,6 +374,7 @@ export default function ChartConfigModal(props: Props) {
                                     ]}
                                   >
                                     <PromqlEditor
+                                      xCluster='Default'
                                       onChange={() => {
                                         setpromeQl(
                                           chartForm
