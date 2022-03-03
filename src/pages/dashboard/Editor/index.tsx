e74804cb4177c7b40805ea9394527169a5fd368e
jsers <xqing9024@gmail.com>
2022-03-03 15:25:39 +0800

    refactor: graph editor styles

--- src/pages/dashboard/Editor/index.tsx
+++ src/pages/dashboard/Editor/index.tsx
@@ -3,6 +3,7 @@ import { Modal, Form, Input, Row, Col, Select, Space, Collapse } from 'antd';
 import { MinusCircleOutlined, PlusCircleOutlined, CloseOutlined } from '@ant-design/icons';
 import _ from 'lodash';
 import { useTranslation } from 'react-i18next';
+import { useSize } from 'ahooks';
 import DateRangePicker, { Range } from '@/components/DateRangePicker';
 import PromqlEditor from '@/components/PromqlEditor';
 import Resolution from '@/components/Resolution';
@@ -17,13 +18,12 @@ interface IProps {
   initialValues: Chart | null;
 }
 
-const WIDTH = '90%';
 const { Panel } = Collapse;
-const namePrefix = ['options', 'tooltip'];
 
 function index(props: ModalWrapProps & IProps) {
   const { t } = useTranslation();
   const { visible, initialValues } = props;
+  const size = useSize(document.querySelector('body'));
   const [chartForm] = Form.useForm();
   const [range, setRange] = useState<Range>({
     description: '小时',
@@ -75,10 +75,9 @@ function index(props: ModalWrapProps & IProps) {
         },
         initialValues,
       )}
-      id='dashboard-panel-form'
     >
       <Modal
-        width={WIDTH}
+        width='100%'
         title={
           <div style={{ display: 'flex', alignItems: 'center' }}>
             <div>{initialValues ? t('编辑图表') : t('新建图表')}</div>
@@ -105,6 +104,7 @@ function index(props: ModalWrapProps & IProps) {
             </Space>
           </div>
         }
+        style={{ top: 10, padding: 0 }}
         visible={visible}
         closable={false}
         footer={null}
@@ -114,120 +114,126 @@ function index(props: ModalWrapProps & IProps) {
         bodyStyle={{
           padding: '10px 24px 24px 24px',
         }}
-        getContainer={() => document.getElementById('dashboard-panel-form')!}
       >
-        <Row
-          gutter={20}
+        <div
           style={{
-            flexWrap: 'nowrap',
+            height: size.height ? size.height - 120 : 500,
           }}
         >
-          <Col flex={1}>
-            <Form.Item noStyle shouldUpdate={(prevValues, curValues) => !_.isEqual(prevValues.targets, curValues.targets)}>
-              {({ getFieldValue }) => {
-                const targets = getFieldValue('targets');
-                return <Renderer time={range} step={step} targets={targets} />;
-              }}
-            </Form.Item>
-            <div style={{ marginTop: 20 }}>
-              <Form.List name='targets'>
-                {(fields, { add, remove }, { errors }) => {
-                  const currentTargets = chartForm.getFieldValue('targets');
-                  const activeKey: string[] = [];
-                  _.forEach(currentTargets, (item, idx) => {
-                    if (item.expaned) {
-                      activeKey.push(_.toString(idx));
-                    }
-                  });
-                  return (
-                    <>
-                      <Collapse
-                        activeKey={activeKey}
-                        onChange={(keys) => {
-                          chartForm.setFieldsValue({
-                            targets: _.map(chartForm.getFieldValue('targets'), (item, idx) => {
-                              return {
-                                ...item,
-                                expaned: _.includes(keys, _.toString(idx)),
-                              };
-                            }),
-                          });
-                        }}
-                      >
-                        {fields.length ? (
-                          fields.map(({ key, name, fieldKey, ...restField }, index) => {
-                            return (
-                              <Panel header={`Query ${index}`} key={index}>
-                                <Form.Item
-                                  label='PromQL'
-                                  name={[name, 'expr']}
-                                  validateTrigger={['onBlur']}
-                                  rules={[
-                                    {
-                                      required: true,
-                                      message: '请输入PromQL',
-                                    },
-                                  ]}
-                                >
-                                  <PromqlEditorField key={name + fieldKey} name={name} fields={fields} index={index} remove={remove} add={add} />
-                                </Form.Item>
-                                <Form.Item
-                                  label='Legend'
-                                  name={[name, 'Legend']}
-                                  tooltip={{
-                                    getPopupContainer: () => document.body,
-                                    title:
-                                      'Controls the name of the time series, using name or pattern. For example {{hostname}} will be replaced with label value for the label hostname.',
-                                  }}
-                                >
-                                  <Input />
-                                </Form.Item>
-                              </Panel>
-                            );
-                          })
-                        ) : (
-                          <PlusCircleOutlined
-                            onClick={() => {
-                              add({
-                                PromQL: '',
-                                expaned: true,
-                              });
-                            }}
-                          />
-                        )}
-                        <Form.ErrorList errors={errors} />
-                      </Collapse>
-                    </>
-                  );
+          <Row
+            gutter={20}
+            style={{
+              flexWrap: 'nowrap',
+              height: '100%',
+            }}
+          >
+            <Col flex={1}>
+              <Form.Item noStyle shouldUpdate={(prevValues, curValues) => !_.isEqual(prevValues.targets, curValues.targets)}>
+                {({ getFieldValue }) => {
+                  const targets = getFieldValue('targets');
+                  return <Renderer time={range} step={step} targets={targets} />;
                 }}
-              </Form.List>
-            </div>
-          </Col>
-          <Col flex='600px'>
-            <N9eCollapse>
-              <N9ePanel header='面板配置'>
-                <>
-                  <Form.Item
-                    label={'标题'}
-                    name='name'
-                    rules={[
-                      {
-                        required: true,
-                        message: '图表名称',
-                      },
-                    ]}
-                  >
-                    <Input />
-                  </Form.Item>
-                  <Form.Item label={'下钻链接'} name='link'>
-                    <Input />
-                  </Form.Item>
-                </>
-              </N9ePanel>
-              <Options />
-            </N9eCollapse>
-          </Col>
-        </Row>
+              </Form.Item>
+              <div style={{ marginTop: 20, height: 'calc(100% - 220px)', overflowY: 'auto' }}>
+                <Form.List name='targets'>
+                  {(fields, { add, remove }, { errors }) => {
+                    const currentTargets = chartForm.getFieldValue('targets');
+                    const activeKey: string[] = [];
+                    _.forEach(currentTargets, (item, idx) => {
+                      if (item.expaned) {
+                        activeKey.push(_.toString(idx));
+                      }
+                    });
+                    return (
+                      <>
+                        <Collapse
+                          activeKey={activeKey}
+                          onChange={(keys) => {
+                            chartForm.setFieldsValue({
+                              targets: _.map(chartForm.getFieldValue('targets'), (item, idx) => {
+                                return {
+                                  ...item,
+                                  expaned: _.includes(keys, _.toString(idx)),
+                                };
+                              }),
+                            });
+                          }}
+                        >
+                          {fields.length ? (
+                            fields.map(({ key, name, fieldKey, ...restField }, index) => {
+                              return (
+                                <Panel header={`Query ${index}`} key={index}>
+                                  <Form.Item
+                                    label='PromQL'
+                                    name={[name, 'expr']}
+                                    validateTrigger={['onBlur']}
+                                    rules={[
+                                      {
+                                        required: true,
+                                        message: '请输入PromQL',
+                                      },
+                                    ]}
+                                  >
+                                    <PromqlEditorField key={name + fieldKey} name={name} fields={fields} index={index} remove={remove} add={add} />
+                                  </Form.Item>
+                                  <Form.Item
+                                    label='Legend'
+                                    name={[name, 'Legend']}
+                                    tooltip={{
+                                      getPopupContainer: () => document.body,
+                                      title:
+                                        'Controls the name of the time series, using name or pattern. For example {{hostname}} will be replaced with label value for the label hostname.',
+                                    }}
+                                  >
+                                    <Input />
+                                  </Form.Item>
+                                </Panel>
+                              );
+                            })
+                          ) : (
+                            <PlusCircleOutlined
+                              onClick={() => {
+                                add({
+                                  PromQL: '',
+                                  expaned: true,
+                                });
+                              }}
+                            />
+                          )}
+                          <Form.ErrorList errors={errors} />
+                        </Collapse>
+                      </>
+                    );
+                  }}
+                </Form.List>
+              </div>
+            </Col>
+            <Col flex='600px' style={{ overflowY: 'auto' }}>
+              <N9eCollapse>
+                <N9ePanel header='面板配置'>
+                  <>
+                    <Form.Item
+                      label={'标题'}
+                      name='name'
+                      rules={[
+                        {
+                          required: true,
+                          message: '图表名称',
+                        },
+                      ]}
+                    >
+                      <Input />
+                    </Form.Item>
+                    <Form.Item label={'下钻链接'} name='link'>
+                      <Input />
+                    </Form.Item>
+                  </>
+                </N9ePanel>
+                <Options />
+              </N9eCollapse>
+            </Col>
+          </Row>
+        </div>
       </Modal>
     </Form>
   );
