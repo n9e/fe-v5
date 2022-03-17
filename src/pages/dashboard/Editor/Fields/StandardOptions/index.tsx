84f9125ea5d34e19103c7060e9893a1cb3046893
jsers <xqing9024@gmail.com>
2022-03-17 17:41:09 +0800

    refactor: change name of the humanize util

--- src/pages/dashboard/Editor/Fields/StandardOptions/index.tsx
+++ src/pages/dashboard/Editor/Fields/StandardOptions/index.tsx
@@ -26,8 +26,8 @@ export default function index() {
               <Option value='percentUnit'>百分比(0.0-1.0)</Option>
             </OptGroup>
             <OptGroup label='Human time duration'>
-              <Option value='humantimeSeconds'>Seconds</Option>
-              <Option value='humantimeMilliseconds'>Milliseconds</Option>
+              <Option value='humantimeSeconds'>Humanize(seconds)</Option>
+              <Option value='humantimeMilliseconds'>Humanize(milliseconds)</Option>
             </OptGroup>
           </Select>
         </Form.Item>
