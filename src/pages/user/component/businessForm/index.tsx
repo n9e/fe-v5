823bc8315c5571015524fc2fe5bde918982cd95a
guguji5 <604821528@qq.com>
2022-03-28 10:18:20 +0800

    feat: 业务组增加英文标识

--- src/pages/user/component/businessForm/index.tsx
+++ src/pages/user/component/businessForm/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useImperativeHandle, ReactNode, useCallback } from 'react';
-import { Form, Input, Select, Switch, Row, Col, Space, Button } from 'antd';
+import { Form, Input, Select, Switch, Row, Tag, Space, Button } from 'antd';
 import { layout } from '../../const';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import { getBusinessTeamInfo, getTeamInfoList } from '@/services/manage';
@@ -14,10 +14,13 @@ const TeamForm = React.forwardRef<ReactNode, TeamProps>((props, ref) => {
   const [form] = Form.useForm();
   const [userTeam, setUserTeam] = useState<Team[]>([]);
   const [initialValues, setInitialValues] = useState({
+    label_enable: false,
+    label_value: '',
     members: [{ perm_flag: true }],
     name: '',
   });
   const [loading, setLoading] = useState<boolean>(true);
+  const [refresh, setRefresh] = useState(true);
   useImperativeHandle(ref, () => ({
     form: form,
   }));
@@ -31,9 +34,11 @@ const TeamForm = React.forwardRef<ReactNode, TeamProps>((props, ref) => {
   }, []);
 
   const getTeamInfoDetail = (id: string) => {
-    getBusinessTeamInfo(id).then((data: { name: string; user_groups: { perm_flag: string; user_group: { id: number } }[] }) => {
+    getBusinessTeamInfo(id).then((data: { name: string; label_enable: number; label_value: string; user_groups: { perm_flag: string; user_group: { id: number } }[] }) => {
       setInitialValues({
         name: data.name,
+        label_enable: data.label_enable === 1,
+        label_value: data.label_value,
         members: data.user_groups.map((item) => ({
           perm_flag: item.perm_flag === 'rw',
           user_group_id: item.user_group.id,
@@ -56,20 +61,62 @@ const TeamForm = React.forwardRef<ReactNode, TeamProps>((props, ref) => {
   const debounceFetcher = useCallback(debounce(getList, 800), []);
 
   return !loading ? (
-    <Form {...layout} form={form} initialValues={initialValues} preserve={false}>
+    <Form {...layout} form={form} initialValues={initialValues} preserve={false} layout={refresh ? 'horizontal' : 'horizontal'}>
       {action !== ActionType.AddBusinessMember && (
-        <Form.Item
-          label={t('业务组名称')}
-          name='name'
-          rules={[
-            {
-              required: true,
-              message: t('业务组名称不能为空！'),
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+        <>
+          <Form.Item
+            label={t('业务组名称')}
+            name='name'
+            rules={[
+              {
+                required: true,
+                message: t('业务组名称不能为空！'),
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label={t('作为标签使用')}
+            name='label_enable'
+            valuePropName='checked'
+            tooltip={{ title: '系统会自动把业务组的英文标识作为标签附到该业务组下辖监控对象的时序数据上', getPopupContainer: () => document.body }}
+          >
+            <Switch />
+          </Form.Item>
+
+          <Form.Item noStyle shouldUpdate={(prevValues, curValues) => prevValues.label_enable !== curValues.label_enable}>
+            {({ getFieldValue }) => {
+              return (
+                getFieldValue('label_enable') && (
+                  <Form.Item
+                    label={t('英文标识')}
+                    name='label_value'
+                    rules={[
+                      {
+                        required: true,
+                      },
+                    ]}
+                    tooltip={{
+                      title: (
+                        <span>
+                          尽量用英文，不能与其他业务组标识重复，系统会自动生成 <Tag color='blue'>busigroup={form.getFieldValue('label_value')}</Tag> 的标签
+                        </span>
+                      ),
+                      getPopupContainer: () => document.body,
+                    }}
+                  >
+                    <Input
+                      onChange={(val) => {
+                        setRefresh(!refresh);
+                      }}
+                    />
+                  </Form.Item>
+                )
+              );
+            }}
+          </Form.Item>
+        </>
       )}
 
       {(action === ActionType.CreateBusiness || action === ActionType.AddBusinessMember) && (
