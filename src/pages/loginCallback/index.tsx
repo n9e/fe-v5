4b6a56a0e4958ef2f3a45fa1bf53ee49c8a1c7c8
jsers <xqing9024@gmail.com>
2022-03-29 11:13:29 +0800

    refactor: 第三方登录成功设置 token

--- src/pages/loginCallback/index.tsx
+++ src/pages/loginCallback/index.tsx
@@ -16,7 +16,9 @@ export default function index() {
     })
       .then((res) => {
         if (res.err === '') {
-          if (res.dat && res.dat.redirect) {
+          if (res.dat) {
+            localStorage.setItem('access_token', res.dat.access_token);
+            localStorage.setItem('refresh_token', res.dat.refresh_token);
             window.location.href = res.dat.redirect;
           }
         } else {
