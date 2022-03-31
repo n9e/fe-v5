1c4e40c9317f4fafe132c6a6d9ff2525b580f15f
jsers <xqing9024@gmail.com>
2022-03-31 14:05:11 +0800

    refactor: query legend 修复不支持下划线，兼容大括号内带空格的写法

--- src/pages/dashboard/Renderer/utils/replaceExpressionBracket.ts
+++ src/pages/dashboard/Renderer/utils/replaceExpressionBracket.ts
@@ -1,15 +1,17 @@
+import _ from 'lodash';
 function extractBracketValue(str) {
-  let reg = /{{([0-9a-zA-Z]+?)}}/g;
+  let reg = /{{([\s0-9a-zA-Z_]+?)}}/g;
   let matchAll: any[] = Array.from(str.matchAll(reg));
   // https://javascript.info/regexp-methods#str-matchall-regexp
   return matchAll.map((item) => (item.length > 0 ? item[1] : item[0]));
 }
 
 export default function replaceExpressionBracket(titleFormat, serieMetricLabels) {
-  let keys = extractBracketValue(titleFormat);
+  let keys = _.map(extractBracketValue(titleFormat), _.trim);
   var legendName = titleFormat;
   keys.forEach((key) => {
-    legendName = legendName.replace(`{{${key}}}`, serieMetricLabels[key] ? serieMetricLabels[key] : '');
+    const reg = new RegExp(`{{\\s?${key}\\s?}}`, 'g');
+    legendName = legendName.replace(reg, serieMetricLabels[key] ? serieMetricLabels[key] : '');
   });
   return legendName;
 }
