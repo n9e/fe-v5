773931af94dd207344ab6b7e6b49c6683611e0d4
jsers <xqing9024@gmail.com>
2022-05-21 11:21:08 +0800

    refactor: Optimize hexbin position and format code

--- src/pages/dashboard/Renderer/Renderer/Hexbin/render.ts
+++ src/pages/dashboard/Renderer/Renderer/Hexbin/render.ts
@@ -14,17 +14,12 @@
  * limitations under the License.
  *
  */
-import {
-  transition,
-  min,
-  select,
-  event,
-} from "d3";
-import { hexbin as d3Hexbin } from "d3-hexbin";
+import { transition, min, select, event } from 'd3';
+import { hexbin as d3Hexbin } from 'd3-hexbin';
 import _ from 'lodash';
-import { bestFitElemCountPerRow, getTextSizeForWidthAndHeight, getMapColumnsAndRows } from "./utils";
+import { bestFitElemCountPerRow, getTextSizeForWidthAndHeight, getMapColumnsAndRows } from './utils';
 
-const xmlns = "http://www.w3.org/2000/svg";
+const xmlns = 'http://www.w3.org/2000/svg';
 const minFont = 6;
 function getPlaceHolderElems(rows, columns, len, radius: number) {
   let points: any[] = [];
@@ -36,44 +31,32 @@ function getPlaceHolderElems(rows, columns, len, radius: number) {
   return points;
 }
 function computeTextFontSize(text: string, linesToDisplay: number, textAreaWidth: number, textAreaHeight: number): number {
-  return getTextSizeForWidthAndHeight(
-    text,
-    textAreaWidth,
-    textAreaHeight / linesToDisplay,
-  );
+  return getTextSizeForWidthAndHeight(text, textAreaWidth, textAreaHeight / linesToDisplay);
 }
 
-const div = select("body")
-  .append(function() {
-    return document.createElement("div");
+const div = select('body')
+  .append(function () {
+    return document.createElement('div');
   })
-  .attr("class", "hexbin-tooltip")
-  .style("opacity", 0);
+  .attr('class', 'hexbin-tooltip')
+  .style('opacity', 0);
 
-function renderHoneyComb(
-  svgGroup,
-  data,
-  { width, height, fontAutoScale = true, fontSize = 12 }
-) {
+function renderHoneyComb(svgGroup, data, { width, height, fontAutoScale = true, fontSize = 12 }) {
   const t = transition().duration(750);
   const { columns: mapColumns, rows: mapRows } = getMapColumnsAndRows(width, height, data.length);
-  const hexRadius = Math.floor(
-    min([
-      width / ((mapColumns + 0.5) * Math.sqrt(3)),
-      height / ((mapRows + 1 / 3) * 1.5),
-      width / 7
-    ])
-  );
+  const hexRadius = Math.floor(min([width / ((mapColumns + 0.5) * Math.sqrt(3)), height / ((mapRows + 1 / 3) * 1.5), width / 7]));
+  const hexbinWidth = Math.sin((60 * Math.PI) / 180) * hexRadius * 2;
   const points = getPlaceHolderElems(mapRows, mapColumns, data.length, hexRadius);
-  const offSetX = hexRadius * Math.cos((30 * Math.PI) / 180);
-  const adjustedOffSetX = offSetX + (width - offSetX * 2 * mapColumns - offSetX) / 2;
+  let adjustedOffSetX = (width - hexbinWidth * mapColumns) / 2 + hexbinWidth / 2;
+  if (points.length >= mapColumns * 2) {
+    adjustedOffSetX = (width - hexbinWidth * mapColumns - hexbinWidth / 2) / 2 + hexbinWidth / 2;
+  }
+  const adjustedOffSetY = (height - hexRadius * 2 * mapRows) / 2 + hexRadius;
   const offSetY = hexRadius;
   const hexbin = d3Hexbin().radius(hexRadius);
-  // const zeroHexBin = d3Hexbin().radius(0);
   const translateX = adjustedOffSetX;
-  const translateY = offSetY;
+  const translateY = adjustedOffSetY;
   const hexbinPoints = hexbin(points);
-  const hexbinWidth = Math.sin(60 * Math.PI / 180) * hexRadius * 2;
   const textAreaHeight = hexRadius;
   const textAreaWidth = hexbinWidth * 0.9;
   let activeLabelFontSize = fontSize;
@@ -118,26 +101,23 @@ function renderHoneyComb(
   const valueWithLabelTextAlignment = textAreaHeight / 2 / 2 + activeValueFontSize / 2;
   const labelWithValueTextAlignment = -(textAreaHeight / 2 / 2) + activeLabelFontSize / 2;
 
-  svgGroup
-    .attr("width", width)
-    .attr("height", height)
-    .attr("transform", `translate(${translateX},${translateY})`);
+  svgGroup.attr('width', width).attr('height', height).attr('transform', `translate(${translateX},${translateY})`);
 
-  const hexagons = svgGroup.selectAll(".hexagon").data(hexbinPoints);
+  const hexagons = svgGroup.selectAll('.hexagon').data(hexbinPoints);
   const removeSelection = hexagons.exit().remove();
 
   hexagons
     .enter()
-    .append(function() {
-      const nodeToAdd = document.createElementNS(xmlns, "path");
+    .append(function () {
+      const nodeToAdd = document.createElementNS(xmlns, 'path');
       return nodeToAdd;
     })
-    .attr("class", "hexagon")
+    .attr('class', 'hexagon')
     // TODO: 进入动画暂时关闭
     // .attr("d", function(d) {
     //   return "M" + d.x + "," + d.y + zeroHexBin.hexagon();
     // })
-    .on("mousemove", function(_d, i) {
+    .on('mousemove', function (_d, i) {
       const metricObj = data[i]?.metric;
       const metricName = metricObj?.__name__ || 'value';
       const metricNameRow = `<div><strong>${metricName}: ${data[i]?.value}</strong></div>`;
@@ -145,112 +125,125 @@ function renderHoneyComb(
         return `<div>${key}: ${val}</div>`;
       });
       const content = `${metricNameRow}${labelsRows.join('')}`;
-      div
-        .style("opacity", 0.9);
+      div.style('opacity', 0.9);
       div
         .html(content)
-        .style("left", event.pageX + 10 + "px")
-        .style("top", event.pageY - 28 + "px");
-    }) 
-    .on("mouseout", function() {
-      div
-        .style("opacity", 0);
+        .style('left', event.pageX + 10 + 'px')
+        .style('top', event.pageY - 28 + 'px');
     })
-    .attr("stroke", "white")
-    .attr("stroke-width", "1px")
-    .style("fill", (_d, i) => {
+    .on('mouseout', function () {
+      div.style('opacity', 0);
+    })
+    .attr('stroke', 'white')
+    .attr('stroke-width', '1px')
+    .style('fill', (_d, i) => {
       return data[i]?.color;
     })
-    .style("fill-opacity", 1)
+    .style('fill-opacity', 1)
     .transition(t)
-    .attr("d", function(d) {
-      return "M" + d.x + "," + d.y + hexbin.hexagon();
+    .attr('d', function (d) {
+      return 'M' + d.x + ',' + d.y + hexbin.hexagon();
     });
 
-    hexagons
-      .enter()
-      .append("text")
-      .attr("x", function (d) {
-        return d.x;
-      })
-      .attr("y", function (d) {
-        return d.y + labelWithValueTextAlignment;
-      })
-      .text(function (_d, i) {
-        let name = data[i]?.name;
-        if (isShowEllipses) {
-          name = name.substring(0, numOfChars) + '...';
-          return name;
-        }
+  hexagons
+    .enter()
+    .append('text')
+    .attr('x', function (d) {
+      return d.x;
+    })
+    .attr('y', function (d) {
+      return d.y + labelWithValueTextAlignment;
+    })
+    .text(function (_d, i) {
+      let name = data[i]?.name;
+      if (isShowEllipses) {
+        name = name.substring(0, numOfChars) + '...';
         return name;
-      })
-      .attr("text-anchor", "middle")
-      .attr("alignment-baseline", "central")
-      .style('pointer-events', "none")
-      .style("font-size", activeLabelFontSize + 'px')
-      .style("fill", "white")
-      .each(function(this, d) {
-        d.bbox = this.getBBox();
-      });
+      }
+      return name;
+    })
+    .attr('text-anchor', 'middle')
+    .attr('alignment-baseline', 'central')
+    .style('pointer-events', 'none')
+    .style('font-size', activeLabelFontSize + 'px')
+    .style('fill', 'white')
+    .each(function (this, d) {
+      d.bbox = this.getBBox();
+    });
 
-    if (activeLabelFontSize) {
-      hexagons.enter().insert("rect", "text")
-        .attr("x", function(d){return d.bbox.x - 4})
-        .attr("y", function(d){return d.bbox.y - 4})
-        .attr("rx", 2)
-        .attr("ry", 2)
-        .attr("width", function(d){return d.bbox.width + 8})
-        .attr("height", function(d){return d.bbox.height + 8})
-        .attr("fill-opacity","0.2")
-        .style("fill", "#000")
-        .style('pointer-events', "none");
-    }
-    
+  if (activeLabelFontSize) {
     hexagons
       .enter()
-      .append("text")
-      .attr("x", function (d) {
-        return d.x;
+      .insert('rect', 'text')
+      .attr('x', function (d) {
+        return d.bbox.x - 4;
       })
-      .attr("y", function (d) {
-        return d.y + valueWithLabelTextAlignment;
+      .attr('y', function (d) {
+        return d.bbox.y - 4;
       })
-      .text(function (_d, i) {
-        const value = data[i]?.value;
-        return value;
+      .attr('rx', 2)
+      .attr('ry', 2)
+      .attr('width', function (d) {
+        return d.bbox.width + 8;
       })
-      .attr("text-anchor", "middle")
-      .attr("alignment-baseline", "central")
-      .style("font-size", activeValueFontSize + 'px')
-      .style("fill", "white")
-      .style('pointer-events', "none")
-      .each(function(this, d) {
-        d.bbox = this.getBBox();
-      });
+      .attr('height', function (d) {
+        return d.bbox.height + 8;
+      })
+      .attr('fill-opacity', '0.2')
+      .style('fill', '#000')
+      .style('pointer-events', 'none');
+  }
 
-    if (activeValueFontSize) {
-      hexagons.enter().insert("rect", "text")
-        .attr("x", function(d){return d.bbox.x - 4})
-        .attr("y", function(d){return d.bbox.y - 4})
-        .attr("rx", 2)
-        .attr("ry", 2)
-        .attr("width", function(d){return d.bbox.width + 8})
-        .attr("height", function(d){return d.bbox.height + 8})
-        .attr("fill-opacity","0.2")
-        .style("fill", "#000")
-        .style('pointer-events', "none");
-    }
+  hexagons
+    .enter()
+    .append('text')
+    .attr('x', function (d) {
+      return d.x;
+    })
+    .attr('y', function (d) {
+      return d.y + valueWithLabelTextAlignment;
+    })
+    .text(function (_d, i) {
+      const value = data[i]?.value;
+      return value;
+    })
+    .attr('text-anchor', 'middle')
+    .attr('alignment-baseline', 'central')
+    .style('font-size', activeValueFontSize + 'px')
+    .style('fill', 'white')
+    .style('pointer-events', 'none')
+    .each(function (this, d) {
+      d.bbox = this.getBBox();
+    });
+
+  if (activeValueFontSize) {
+    hexagons
+      .enter()
+      .insert('rect', 'text')
+      .attr('x', function (d) {
+        return d.bbox.x - 4;
+      })
+      .attr('y', function (d) {
+        return d.bbox.y - 4;
+      })
+      .attr('rx', 2)
+      .attr('ry', 2)
+      .attr('width', function (d) {
+        return d.bbox.width + 8;
+      })
+      .attr('height', function (d) {
+        return d.bbox.height + 8;
+      })
+      .attr('fill-opacity', '0.2')
+      .style('fill', '#000')
+      .style('pointer-events', 'none');
+  }
 
   return [...removeSelection.nodes()];
 }
 
-export function renderFn(
-  data,
-  { width, height, parentGroupEl }
-) {
-  const parentGroup = select(parentGroupEl)
-    .attr("width", width)
-    .attr("height", height);
+export function renderFn(data, { width, height, parentGroupEl }) {
+  const parentGroup = select(parentGroupEl).attr('width', width).attr('height', height);
   const countPerRow = bestFitElemCountPerRow(1, width, height);
   const unitWidth = Math.floor(width / countPerRow);
   const rowCount = Math.ceil(1 / countPerRow);
