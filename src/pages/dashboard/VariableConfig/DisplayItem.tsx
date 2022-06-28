c43b54810d5b44b78afa5ec39b5731ca41c0d3a3
jsers <xqing9024@gmail.com>
2022-06-28 16:51:11 +0800

    refactor: variable support text box type

--- src/pages/dashboard/VariableConfig/DisplayItem.tsx
+++ src/pages/dashboard/VariableConfig/DisplayItem.tsx
@@ -15,7 +15,7 @@
  *
  */
 import React, { useState } from 'react';
-import { Select } from 'antd';
+import { Select, Input } from 'antd';
 import _ from 'lodash';
 import { IVariable } from './definition';
 import { getVaraiableSelected, setVaraiableSelected } from './constant';
@@ -28,48 +28,60 @@ interface IProps {
 
 export default function DisplayItem(props: IProps) {
   const { id, expression, onChange } = props;
-  const { name, multi, allOption, options } = expression;
-  const [selected, setSelected] = useState<string[]>(getVaraiableSelected(name, id));
+  const { name, multi, allOption, options, type, defaultValue } = expression;
+  const [selected, setSelected] = useState<string[]>(getVaraiableSelected(name, id) || defaultValue);
 
   return (
     <div className='tag-content-close-item'>
       <div className='tag-content-close-item-tagName'>{name}</div>
-      <Select
-        mode={multi ? 'tags' : undefined}
-        style={{
-          width: '180px',
-        }}
-        onChange={(v) => {
-          let val = v;
-          if (multi && allOption && val.includes('all')) {
-            val = ['all'];
-          } else if (multi && !allOption) {
-            let allIndex = val.indexOf('all');
-            if (allIndex !== -1) {
-              val.splice(allIndex, 1);
+      {type === 'query' ? (
+        <Select
+          mode={multi ? 'tags' : undefined}
+          style={{
+            width: '180px',
+          }}
+          onChange={(v) => {
+            let val = v;
+            if (multi && allOption && val.includes('all')) {
+              val = ['all'];
+            } else if (multi && !allOption) {
+              let allIndex = val.indexOf('all');
+              if (allIndex !== -1) {
+                val.splice(allIndex, 1);
+              }
             }
-          }
-          setVaraiableSelected(name, val, id, true);
-          setSelected(val);
-          onChange();
-        }}
-        defaultActiveFirstOption={false}
-        showSearch
-        value={selected}
-        dropdownClassName='overflow-586'
-      >
-        {allOption && (
-          <Select.Option key={'all'} value={'all'}>
-            all
-          </Select.Option>
-        )}
-        {options &&
-          options.map((value) => (
-            <Select.Option key={value} value={value}>
-              {value}
+            setVaraiableSelected(name, val, id, true);
+            setSelected(val);
+            onChange();
+          }}
+          defaultActiveFirstOption={false}
+          showSearch
+          value={selected}
+          dropdownClassName='overflow-586'
+        >
+          {allOption && (
+            <Select.Option key={'all'} value={'all'}>
+              all
             </Select.Option>
-          ))}
-      </Select>
+          )}
+          {options &&
+            options.map((value) => (
+              <Select.Option key={value} value={value}>
+                {value}
+              </Select.Option>
+            ))}
+        </Select>
+      ) : (
+        <Input
+          value={selected}
+          onChange={(v) => {
+            let val = v.target.value;
+            setVaraiableSelected(name, val, id, true);
+            setSelected(val as any);
+            onChange();
+          }}
+        />
+      )}
     </div>
   );
 }
