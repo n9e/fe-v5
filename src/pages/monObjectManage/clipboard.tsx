2c5f596ea04ee7f2569cd8fb259674f771fd201d
jsers <xqing9024@gmail.com>
2022-07-26 20:26:04 +0800

    refactor: targets table

---
+++ src/pages/monObjectManage/clipboard.tsx
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022 Nightingale Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/**
+ * 将文本添加到剪贴板
+ */
+export default function clipboard(text: string): boolean {
+  const fakeElem = document.createElement('textarea');
+  fakeElem.style.border = '0';
+  fakeElem.style.padding = '0';
+  fakeElem.style.margin = '0';
+  fakeElem.style.position = 'absolute';
+  fakeElem.style.left = '-9999px';
+  const yPosition = window.pageYOffset || document.documentElement.scrollTop;
+  fakeElem.style.top = `${yPosition}px`;
+  fakeElem.setAttribute('readonly', '');
+  fakeElem.value = text;
+
+  document.body.appendChild(fakeElem);
+  fakeElem.select();
+  let succeeded;
+  try {
+    succeeded = document.execCommand('copy');
+  } catch (err) {
+    succeeded = false;
+  }
+  if (succeeded) {
+    document.body.removeChild(fakeElem);
+  }
+  return succeeded;
+}
