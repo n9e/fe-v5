297de76bfac9e26770b067192eb9f89c0458297c
jsers <xqing9024@gmail.com>
2022-09-01 11:12:44 +0800

    stash

--- src/pages/event/LogsDetail/index.tsx
+++ src/pages/event/LogsDetail/index.tsx
@@ -9,6 +9,7 @@ import { json } from '@codemirror/lang-json';
 import { defaultHighlightStyle } from '@codemirror/highlight';
 import TimeRangePicker, { IRawTimeRange, parseRange } from '@/components/TimeRangePicker';
 import { getEventLogQuery } from '@/services/warning';
+import './style.less';
 
 interface IProps {
   visible: boolean;
@@ -20,6 +21,14 @@ interface IJSON {
   [key: string]: string | IJSON;
 }
 
+function JSONParse(val) {
+  let json: IJSON = {};
+  try {
+    json = JSON.parse(val);
+  } catch (e) {}
+  return json;
+}
+
 export default function index(props: IProps) {
   const { id, visible, onClose } = props;
   const [range, setRange] = useState<IRawTimeRange>({
@@ -27,6 +36,7 @@ export default function index(props: IProps) {
     end: 'now',
   });
   const [limit, setLimit] = useState(10);
+  const [fields, setFields] = useState<string[]>([]);
   const [selectedFields, setSelectedFields] = useState<string[]>();
   const [data, setData] = useState<
     {
@@ -45,22 +55,26 @@ export default function index(props: IProps) {
       end,
       limit,
     }).then((res) => {
-      setData(
-        _.map(res.dat, (item) => {
-          return {
-            raw: item,
-            parsed: JSON.parse(item),
-          };
-        }),
-      );
+      let allFields: string[] = [];
+      const newData = _.map(res.dat, (item) => {
+        const json = JSONParse(item);
+        const keys = _.keys(json);
+        allFields = _.union(_.concat(allFields, keys));
+        return {
+          id: _.uniqueId(),
+          raw: item,
+          parsed: json,
+        };
+      });
+      setFields(allFields);
+      setSelectedFields(allFields.slice(0, 5));
+      setData(newData);
     });
   }, [id, JSON.stringify(range), limit]);
 
-  console.log('data', data);
-
   return (
     <Drawer title='日志详情' width={960} placement='right' onClose={onClose} visible={visible}>
-      <div>
+      <div style={{ marginBottom: 10 }}>
         <Space>
           <TimeRangePicker value={range} onChange={setRange} />
           <Input.Group>
@@ -88,30 +102,41 @@ export default function index(props: IProps) {
               }}
               style={{ minWidth: 200 }}
             >
-              <Select.Option value={10}>10</Select.Option>
-              <Select.Option value={20}>20</Select.Option>
-              <Select.Option value={50}>50</Select.Option>
-              <Select.Option value={100}>100</Select.Option>
-              <Select.Option value={500}>500</Select.Option>
+              {fields.map((item) => {
+                return <Select.Option value={item}>{item}</Select.Option>;
+              })}
             </Select>
           </Input.Group>
         </Space>
       </div>
       <Table
+        className='event-logs-table'
+        rowKey='id'
         showHeader={false}
         columns={[
           {
-            dataIndex: 'logs',
+            dataIndex: 'parsed',
             render(text) {
-              return text;
+              return (
+                <div style={{ height: 100, overflow: 'hidden', textOverflow: 'ellipsis', wordBreak: 'break-all' }}>
+                  {_.map(text, (val, key) => {
+                    return (
+                      <span key={key}>
+                        <span>{key}:</span> {typeof val === 'object' ? JSON.stringify(val) : val}
+                      </span>
+                    );
+                  })}
+                </div>
+              );
             },
           },
         ]}
+        dataSource={data}
         expandable={{
           expandedRowRender: (record) => {
             let value = '';
             try {
-              value = JSON.stringify(record.__json__, null, 4);
+              value = JSON.stringify(record.parsed, null, 4);
             } catch (e) {
               console.error(e);
               value = '无法解析';
