66efcdb56c9b7dea50628c21445891847a5b2b36
jsers <xqing9024@gmail.com>
2022-09-20 10:03:45 +0800

    refactor: rm dashboard detail context

--- src/pages/dashboard/Share/index.tsx
+++ src/pages/dashboard/Share/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { DetailReducer } from '../DetailContext';
 import Detail from './Detail';
 
 export default function index2() {
-    return (
-    <DetailReducer>
-      <Detail />
-    </DetailReducer>
-    );
+  return <Detail />;
 }
