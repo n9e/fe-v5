064f5df25c36c4867854428f7a830483a5bcede6
guguji5 <604821528@qq.com>
2022-11-12 20:27:15 +0800

    feat: license到期后 弹窗提示，1分钟自动消失

--- src/pages/warning/strategy/license.tsx
+++ src/pages/warning/strategy/license.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Tooltip } from 'antd';
+import { Tooltip, Modal } from 'antd';
 import _ from 'lodash';
 import moment from 'moment';
 import { getBrainLicense } from '@/services/warning';
@@ -9,11 +9,18 @@ export default function License() {
   const [rules, setRules] = useState<any>();
   useEffect(() => {
     const now = moment().unix();
-    getBrainLicense().then((res) => {
-      setDays(_.round((res?.data?.expire - now) / 86400));
-      setRules(res?.data?.rules_remaining);
-      window.localStorage.setItem('license_rules_remaining', _.toString(res?.data?.rules_remaining));
-    });
+    getBrainLicense()
+      .then((res) => {
+        setDays(_.round((res?.data?.expire - now) / 86400));
+        setRules(res?.data?.rules_remaining);
+        window.localStorage.setItem('license_rules_remaining', _.toString(res?.data?.rules_remaining));
+      })
+      .catch((e) => {
+        const modal = Modal.error({ closable: false, maskClosable: false, title: 'License证书不存在或已过期，请联系Flashcat技术支持', className: 'license-off' });
+        setTimeout(() => {
+          modal.destroy();
+        }, 60000);
+      });
   }, []);
   if (!days) return null;
   if (days > 30) return null;
