02c1510bf884ec9aded6c1b910d37bdb500e833d
jsers <xqing9024@gmail.com>
2022-11-29 09:42:31 +0800

    feat: shield support SLS

--- src/pages/warning/shield/components/CateSelect/index.tsx
+++ src/pages/warning/shield/components/CateSelect/index.tsx
@@ -5,16 +5,17 @@ import { CaretDownOutlined } from '@ant-design/icons';
 const typeNameMap = {
   prometheus: 'Prometheus',
   elasticsearch: 'Elasticsearch',
+  'aliyun-sls': '阿里云SLS',
 };
 
 export default function index({ visible, form }) {
-  const options = visible ? ['prometheus', 'elasticsearch'] : ['prometheus'];
+  const options = visible ? ['prometheus', 'elasticsearch', 'aliyun-sls'] : ['prometheus'];
   return (
     <Form.Item label='数据源类型' name='cate'>
       <Select
         suffixIcon={<CaretDownOutlined />}
-        onChange={(val) => {
-          const values: any = {
+        onChange={() => {
+          const values = {
             cluster: [],
           };
           form.setFieldsValue(values);
