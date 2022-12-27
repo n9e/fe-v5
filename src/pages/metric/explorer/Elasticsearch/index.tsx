d9ab10ae54527c246a5b6cbf363dd4938c51b290
star-6 <290043518@qq.com>
2022-12-27 17:38:14 +0800

    refactor:即时查询中过滤条件增加兼容处理

--- src/pages/metric/explorer/Elasticsearch/index.tsx
+++ src/pages/metric/explorer/Elasticsearch/index.tsx
@@ -34,7 +34,7 @@ export default function index(props: IProps) {
   const filtersArr: string[] = [];
   for (const [key, value] of params) {
     if (!['data_source_id', 'index_name', 'timestamp'].includes(key)) {
-      filtersArr.push(`${key}:${value}`);
+      filtersArr.push(`${key}:"${value}"`);
     }
   }
 
