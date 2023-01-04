bda358d531fe5dd51ba39b7a72b883ecf31e42c5
jsers <xqing9024@gmail.com>
2023-01-04 12:02:52 +0800

    refactor: remove unused component

--- src/pages/demo/index.tsx
+++ src/pages/demo/index.tsx
@@ -16,7 +16,6 @@
  */
 import React, { useRef, useEffect, useState } from 'react';
 import { Form, Input, Button, Radio, Collapse, Row, Col } from 'antd';
-import PromqlEditor from '@/components/PromqlEditor';
 const { Panel } = Collapse;
 import G2PieChart from '@/components/G2PieChart';
 import ColumnSelect from '@/components/ColumnSelect';
@@ -37,7 +36,6 @@ export default function Demo() {
         <Panel header='PromqlEditor' key='1'>
           <Row style={{ marginBottom: 20 }}>
             <p>PromqlEditor 的自动闭合及联想功能不支持动态切换集群，xCluster变化后自己销毁并重新初始化组件</p>
-            <PromqlEditor style={{ width: '100%' }} xCluster={xCluster} />
           </Row>
         </Panel>
         <Panel header='G2PieChart' key='2'>
