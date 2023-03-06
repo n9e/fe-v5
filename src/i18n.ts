94dfe7f64508925c3a7d11f9034f54a7b21791e6
guguji5 <604821528@qq.com>
2023-03-06 15:11:01 +0800

    refactor: 默认中文；请求带上header

--- src/i18n.ts
+++ src/i18n.ts
@@ -26,7 +26,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: localStorage.getItem('language') || 'zh', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: localStorage.getItem('language') || 'zh_CN', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
