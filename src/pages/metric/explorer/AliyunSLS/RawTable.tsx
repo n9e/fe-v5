3e87ec225fe227bf756aff9d4ef8c9abd04a8e91
guguji5 <604821528@qq.com>
2023-03-08 11:15:06 +0800

    bugfix: remove wrong useTranslation

--- src/pages/metric/explorer/AliyunSLS/RawTable.tsx
+++ src/pages/metric/explorer/AliyunSLS/RawTable.tsx
@@ -3,7 +3,7 @@ import { Table, Tag } from 'antd';
 import { DownOutlined, RightOutlined } from '@ant-design/icons';
 import _ from 'lodash';
 import { getColumnsFromFields, getInnerTagKeys } from './utils';
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 interface IProps {
   data: any[];
   selectedFields: string[];
@@ -13,44 +13,57 @@ interface IProps {
   };
 }
 export default function RawTable(props: IProps) {
-  const {
-    t
-  } = useTranslation();
-  const {
-    data,
-    selectedFields,
-    scroll
-  } = props;
+  const { t } = useTranslation();
+  const { data, selectedFields, scroll } = props;
   const [isMore, setIsMore] = useState(true);
-  return <Table size='small' className='event-logs-table' tableLayout='auto' rowKey={record => {
-    return _.join(_.map(record, val => val), '-');
-  }} columns={getColumnsFromFields(selectedFields, '__time__')} dataSource={data} expandable={{
-    expandedRowRender: record => {
-      const {
-        t
-      } = useTranslation();
-      const tagskeys = getInnerTagKeys(record);
-      return <div className='sls-discover-raw-content'>
-              {!_.isEmpty(tagskeys) && <div className='sls-discover-raw-tags'>
-                  {_.map(tagskeys, key => {
-            return <Tag color='purple'>{record[key]}</Tag>;
-          })}
-                </div>}
+  return (
+    <Table
+      size='small'
+      className='event-logs-table'
+      tableLayout='auto'
+      rowKey={(record) => {
+        return _.join(
+          _.map(record, (val) => val),
+          '-',
+        );
+      }}
+      columns={getColumnsFromFields(selectedFields, '__time__')}
+      dataSource={data}
+      expandable={{
+        expandedRowRender: (record) => {
+          const tagskeys = getInnerTagKeys(record);
+          return (
+            <div className='sls-discover-raw-content'>
+              {!_.isEmpty(tagskeys) && (
+                <div className='sls-discover-raw-tags'>
+                  {_.map(tagskeys, (key) => {
+                    return <Tag color='purple'>{record[key]}</Tag>;
+                  })}
+                </div>
+              )}
 
               {_.map(_.omit(record, tagskeys), (val, key) => {
-          return <dl key={key} className='event-logs-row'>
+                return (
+                  <dl key={key} className='event-logs-row'>
                     <dt>{key}: </dt>
                     <dd>{val}</dd>
-                  </dl>;
-        })}
-            </div>;
-    },
-    expandIcon: ({
-      expanded,
-      onExpand,
-      record
-    }) => expanded ? <DownOutlined onClick={e => onExpand(record, e)} /> : <RightOutlined onClick={e => onExpand(record, e)} />
-  }} pagination={false} scroll={scroll} footer={!isMore ? () => {
-    return t("只能查询您搜索匹配的前 500 个日志，请细化您的过滤条件。");
-  } : undefined} />;
+                  </dl>
+                );
+              })}
+            </div>
+          );
+        },
+        expandIcon: ({ expanded, onExpand, record }) => (expanded ? <DownOutlined onClick={(e) => onExpand(record, e)} /> : <RightOutlined onClick={(e) => onExpand(record, e)} />),
+      }}
+      pagination={false}
+      scroll={scroll}
+      footer={
+        !isMore
+          ? () => {
+              return t('只能查询您搜索匹配的前 500 个日志，请细化您的过滤条件。');
+            }
+          : undefined
+      }
+    />
+  );
 }
