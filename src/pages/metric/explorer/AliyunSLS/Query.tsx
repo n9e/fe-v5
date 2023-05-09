b3ff3969603eb039f132764e964ad2aae2f24767
jsers <xqing9024@gmail.com>
2023-05-09 18:27:24 +0800

    docs: rollback the tip content of the filter

--- src/pages/metric/explorer/AliyunSLS/Query.tsx
+++ src/pages/metric/explorer/AliyunSLS/Query.tsx
@@ -14,9 +14,23 @@ interface IProps {
 }
 export default function QueryInput(props: IProps) {
   const { t } = useTranslation();
-  const content = `${t(
-    "\n场景举例  \n查询日志原文:  \n- 查询GET、POST请求成功的日志：(request_method:GET or request_method:POST) and status in [200 299]\n- 查询GET、POST请求失败的日志：(request_method:GET or request_method:POST) and status in [200 299]\n- 查询request_uri字段值以/request开头的日志：request_uri:/request*  \n\n查询时序值:  \n- 查询GET、POST请求成功的日志条数，有两种查询方式，如果对展示的时间样式没有特殊要求，可以不写 [time_series](https://help.aliyun.com/document_detail/63451.htm)\n  - (request_method:GET or request_method:POST) and status in [200 299]|count(1) as count\n  - (request_method:GET or request_method:POST) and status in [200 299]|count(1) as count, select time_series(__time__, '1m', '%H:%i:%s' ,'0') as Time group by Time order by Time limit 100\n\n详细文档：\n- [查询语法](https://help.aliyun.com/document_detail/29060.htm) \n- [分析语法](https://help.aliyun.com/document_detail/53608.html)\n- [函数概览](https://help.aliyun.com/document_detail/321454.html)",
-  )}`;
+  const content = `
+  场景举例  
+  查询日志原文:  
+  - 查询GET、POST请求成功的日志：(request_method:GET or request_method:POST) and status in [200 299]
+  - 查询GET、POST请求失败的日志：(request_method:GET or request_method:POST) and status in [200 299]
+  - 查询request_uri字段值以/request开头的日志：request_uri:/request*  
+
+  查询时序值:  
+  - 查询GET、POST请求成功的日志条数，有两种查询方式，如果对展示的时间样式没有特殊要求，可以不写 [time_series](https://help.aliyun.com/document_detail/63451.htm)
+    - (request_method:GET or request_method:POST) and status in [200 299]|count(1) as count
+    - (request_method:GET or request_method:POST) and status in [200 299]|count(1) as count, select time_series(__time__, '1m', '%H:%i:%s' ,'0') as Time group by Time order by Time limit 100
+
+  详细文档：
+  - [查询语法](https://help.aliyun.com/document_detail/29060.htm) 
+  - [分析语法](https://help.aliyun.com/document_detail/53608.html)
+  - [函数概览](https://help.aliyun.com/document_detail/321454.html)
+  `;
   const { prefixField = {}, prefixName = [], width = 84, layout = 'horizontal', required = false } = props;
   const label = (
     <span>
